17f92bc4d9de198fc8258d3ecf79969806641eda
VitaliySotskov <vit6150@gmail.com>
2023-08-18 02:13:04 +0300

    module1-task3

---
+++ src/view/list-view.js
@@ -0,0 +1,24 @@
+import { createElement } from '../render.js';
+
+function createListTemplate () {
+  return /*html*/ `
+    <ul class="trip-events__list"></ul>`;
+}
+
+export default class ListView {
+  getTemplate() {
+    return createListTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
