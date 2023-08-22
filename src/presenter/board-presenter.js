7cb702333ac71b21c72c4811fcfad1389a408cdf
VitaliySotskov <vit6150@gmail.com>
2023-08-23 00:10:32 +0300

    Мелкие исправления

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -3,7 +3,6 @@ import ListView from '../view/list-view.js';
 import EventEditView from '../view/event-edit-view.js';
 import PointView from '../view/point-view.js';
 import { render } from '../render.js';
-
 export default class BoardPresenter {
   listComponent = new ListView();
 
@@ -15,7 +14,6 @@ export default class BoardPresenter {
     render(new EventEditView(), this.listComponent.getElement());
     render(new SortView(), this.container);
     render(this.listComponent, this.container);
-
 
     for (let i = 0; i < 3; i++) {
       render(new PointView(), this.listComponent.getElement());
