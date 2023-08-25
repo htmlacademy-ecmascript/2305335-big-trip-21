7af8792cf26b6456c18815e3b4677fc6d8612884
VitaliySotskov <vit6150@gmail.com>
2023-08-25 21:25:03 +0300

    Исправление презентера

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -3,20 +3,38 @@ import ListView from '../view/list-view.js';
 import EventEditView from '../view/event-edit-view.js';
 import PointView from '../view/point-view.js';
 import { render } from '../render.js';
+//import DestinationsModel from '../model/destination-model.js';
 export default class BoardPresenter {
   listComponent = new ListView();
 
-  constructor({ container }) {
+  constructor({ container, destinationsModel, offersModel, pointsModel }) {
     this.container = container;
+    this.destinationsModel = destinationsModel;
+    this.offersModel = offersModel;
+    this.pointsModel = pointsModel;
+    this.points = [...pointsModel.get()];
   }
 
   init() {
-    render(new EventEditView(), this.listComponent.getElement());
-    render(new SortView(), this.container);
     render(this.listComponent, this.container);
+    render(new EventEditView({
+      point: this.points[0],
+      pointDestination: this.destinationsModel.get(),
+      pointOffers: this.offersModel.get()
+    }),
+    this.listComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new PointView(), this.listComponent.getElement());
-    }
+    render(new SortView(), this.container);
+
+    this.points.forEach((point) => {
+      render(
+        new PointView({
+          point,
+          pointDestination: this.destinationsModel.getById(point.destination),
+          pointOffers: this.offersModel.getByType(point.type)
+        }),
+        this.pointsListComponent.getElement()
+      );
+    });
   }
 }
