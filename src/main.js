a44eee6f3e77a4c72bcc5f72cf196c7b8a50f2ad
VitaliySotskov <vit6150@gmail.com>
2023-09-02 20:59:00 +0300

    Корректировки для отображения пустой страницы

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import FilterView from './view/filter-view/filter-view.js';
-import TripInfoView from './view/trip-info-view/trip-info-view.js';
 import PointsPresenter from './presenter/points-presenter.js';
 
 import PointsModel from './model/points-model.js';
@@ -7,10 +5,6 @@ import OffersModel from './model/offers-model.js';
 import DestinationsModel from './model/destination-model.js';
 import MockService from './service/mock-service.js';
 
-import { RenderPosition, render } from './framework/render.js';
-
-const filterElement = document.querySelector('.trip-controls__filters');
-const infoTripElement = document.querySelector('.trip-main');
 const eventsListElement = document.querySelector('.trip-events');
 
 const mockService = new MockService();
@@ -24,8 +18,5 @@ const pointsPresenter = new PointsPresenter({
   offersModel,
   destinationsModel
 });
-
-render(new TripInfoView(), infoTripElement, RenderPosition.AFTERBEGIN);
-render(new FilterView(), filterElement);
 
 pointsPresenter.init();
--- src/view/points-view/list-empty-template.js
+++ src/view/points-view/list-empty-template.js
@@ -1,18 +1,6 @@
-
 function createListEmptyTemplate() {
-  return (/*html*/
-    `<section class="trip-events">
-      <h2 class="visually-hidden">Trip events</h2>
-      <p class="trip-events__msg">Click New Event to create your first point</p>
-      <!--
-        Значение отображаемого текста зависит от выбранного фильтра:
-          * Everthing – 'Click New Event to create your first point'
-          * Past — 'There are no past events now';
-          * Present — 'There are no present events now';
-          * Future — 'There are no future events now'.
-      -->
-    </section>`
-  );
+  return /*html*/`
+  <p class="trip-events__msg">Click New Event to create your first point</p>`;
 }
 
 export { createListEmptyTemplate };
